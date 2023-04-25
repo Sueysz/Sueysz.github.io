73d32863721d3fde23c8448474cb6bedc81af7e2
sueys <sueys@live.fr>
2023-04-25 21:55:30 +0200

    Add moon also change colorbg add todolistreact

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const toggleBtn = document.getElementById('dark-btn');
 const body = document.body;
 
-toggleBtn.addEventListener('click', function() {
+toggleBtn.addEventListener('click', () =>{
   body.classList.toggle('dark-mode');
+  if (body.classList.contains('dark-mode')) {
+    
+    const moonIcon = document.createElement('span');
+    moonIcon.id = 'dark-btn';
+    moonIcon.innerHTML = '🌙';
+    toggleBtn.parentNode.replaceChild(moonIcon, toggleBtn);
+  } else {
+    
+    const sunIcon = document.createElement('span');
+    sunIcon.id = 'dark-btn';
+    sunIcon.innerHTML = '☀️';
+    toggleBtn.parentNode.replaceChild(sunIcon, toggleBtn);
+  }
 });
